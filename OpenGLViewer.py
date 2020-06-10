66803d4c9b9846c90d082eaa428b39b6a4d2c9a5
fschm002 <florian@krygier-schmidt.de>
2020-06-11 01:31:12 +0200

    "Color Keys + Shift"-Fehler behoben, es wurden immer beide Farben geändert; mit gedrücktem Scrollrad rein- und rauszoomen

--- OpenGLViewer.py
+++ OpenGLViewer.py
@@ -66,6 +66,7 @@ class OpenGLViewer:
         glfw.set_mouse_button_callback(self.window, self.onMouseButton)
         glfw.set_key_callback(self.window, self.onKeyboard)
         glfw.set_window_size_callback(self.window, self.onSize)
+        glfw.set_cursor_pos_callback(self.window, self.onMouseMove)
 
         # create 3D
         self.scene = Scene(self.width, self.height)
@@ -76,8 +77,30 @@ class OpenGLViewer:
         # animation flag
         self.animation = True
 
+        # mouse
+        self.startZoomPoint = (0, 0)
+        self.middleMouseClicked = False
+        self.lasty = 0
+
     def onMouseButton(self, win, button, action, mods):
         print("mouse button: ", win, button, action, mods)
+        if button == glfw.MOUSE_BUTTON_MIDDLE:
+            if action == glfw.PRESS:
+                self.middleMouseClicked = True
+            elif action == glfw.RELEASE:
+                self.middleMouseClicked = False
+                # self.angle = 0
+            print("heran- bzw. wegzoomen")
+
+    def onMouseMove(self, win, posX, posY):
+        r = min(self.width, self.height) / 2.0
+        if self.middleMouseClicked:
+            y = posY - self.startZoomPoint[1]
+            if self.lasty - y > 0:
+                glScale(0.9, 0.9, 0.9)
+            else:
+                glScale(1.1, 1.1, 1.1)
+            self.lasty = y
 
     def onKeyboard(self, win, key, scancode, action, mods):
         print("keyboard: ", win, key, scancode, action, mods)
@@ -98,34 +121,38 @@ class OpenGLViewer:
                 self.scene.objectCol = (0.05, 0.6, 1.0, 1.0)
 
             # Change Color to Black
-            if key == glfw.KEY_S:
+            if key == glfw.KEY_S and not mods == glfw.MOD_SHIFT:
                 self.scene.backgroundCol = (0.0, 0.0, 0.0, 0.0)
             if key == glfw.KEY_S and mods == glfw.MOD_SHIFT:
                 self.scene.objectCol = (0.0, 0.0, 0.0, 0.0)
 
             # Change Color to White
-            if key == glfw.KEY_W:
+            if key == glfw.KEY_W and not mods == glfw.MOD_SHIFT:
                 self.scene.backgroundCol = (1.0, 1.0, 1.0, 0.0)
             if key == glfw.KEY_W and mods == glfw.MOD_SHIFT:
                 self.scene.objectCol = (1.0, 1.0, 1.0, 0.0)
 
             # Change Color to Red
-            if key == glfw.KEY_R:
+            if key == glfw.KEY_R and not mods == glfw.MOD_SHIFT:
                 self.scene.backgroundCol = (1.0, 0.0, 0.0, 0.0)
             if key == glfw.KEY_R and mods == glfw.MOD_SHIFT:
                 self.scene.objectCol = (1.0, 0.0, 0.0, 0.0)
 
             # Change Color to Yellow
-            if key == glfw.KEY_G:
+            if key == glfw.KEY_G and not mods == glfw.MOD_SHIFT:
                 self.scene.backgroundCol = (1.0, 1.0, 0.0, 0.0)
             if key == glfw.KEY_G and mods == glfw.MOD_SHIFT:
                 self.scene.objectCol = (1.0, 1.0, 0.0, 0.0)
 
             # Change Color to Blue
-            if key == glfw.KEY_B:
+            if key == glfw.KEY_B and not mods == glfw.MOD_SHIFT:
                 self.scene.backgroundCol = (0.0, 0.0, 1.0, 0.0)
             if key == glfw.KEY_B and mods == glfw.MOD_SHIFT:
                 self.scene.objectCol = (0.0, 0.0, 1.0, 0.0)
+
+            # Enable/Disable Shadow
+            if key == glfw.KEY_H:
+                print("Schatten an/aus")
 
     def onSize(self, win, width, height):
         print("onsize: ", win, width, height)
